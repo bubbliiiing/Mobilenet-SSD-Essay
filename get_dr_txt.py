4c14496df35ff072fdd8ae51d6d5eb16a937a5d5
Bubbliiiing <47347516+bubbliiiing@users.noreply.github.com>
2020-09-10 11:42:40 +0800

    Update get_dr_txt.py

--- get_dr_txt.py
+++ get_dr_txt.py
@@ -65,7 +65,7 @@ if not os.path.exists("./input/images-optional"):
 for image_id in tqdm(image_ids):
     image_path = "./VOCdevkit/VOC2007/JPEGImages/"+image_id+".jpg"
     image = Image.open(image_path)
-    image.save("./input/images-optional/"+image_id+".jpg")
+    # image.save("./input/images-optional/"+image_id+".jpg")
     ssd.detect_image(image_id,image)
     
 
